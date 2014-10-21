2eb74f1123ec638a816f68bbad3a057b364d0f13
shine_forever <shine_forever@yeah.net>
2014-10-21 14:52:41 +0800

    写一个活动系统操作系统发现版本名称，主机名，内核版本脚本

---
+++ host_info.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+import platform
+
+
+def getHostInfo():
+    """
+    获得系统的发行版本名称，版本，以及安装操作系统的32位或者64位；
+    """
+    pd = {}
+    version = platform.dist()
+    os_name = platform.node()
+    os_release = platform.release()
+    os_version = ' %s %s' % (version[0],version[1])
+    pd['os_name'] = os_name
+    pd['os_release'] = os_release
+    pd['os_version'] = os_version
+    return pd
+
+if __name__ == '__main__':
+    print getHostInfo()
+    
